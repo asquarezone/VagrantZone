d274333b57a08dad1cfaf6472747c56b94379c1e
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2016-10-21 17:06:41 +0530

    Made Changes

---
+++ Jenkins_Ubuntu/provision.sh
@@ -0,0 +1,23 @@
+sudo apt-get -y update
+
+sudo apt-get -y install software-properties-common
+
+sudo apt-add-repository ppa:webupd8team/java
+
+sudo apt-get -y update
+
+sudo apt-get -y install oracle-java8-installer
+
+sudo apt-get -y install oracle-java8-set-default
+
+
+#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+
+#dpkg -i  https://pkg.jenkins.io/debian-stable binary/
+
+#sudo apt-get -y update
+#sudo apt-get -y install jenkins
+
+
+
+
